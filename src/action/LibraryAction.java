d36343d33f3fdf9a0cef96805be7e1852c3ded84
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-07-21 16:20:31 +0530

    Created LibraryAction Interface

---
+++ src/action/LibraryAction.java
@@ -0,0 +1,25 @@
+package action;
+
+import domain.Book;
+import domain.Student;
+
+public interface LibraryAction {
+    void addBook(Book book);
+
+    void addStudent(Student student);
+
+    void issueBook(int studentID, String title);
+
+    void returnBook(String title);
+
+    void listAllBooks();
+
+    void listAllStudents();
+
+    void searchBookByTittle(String title);
+
+    void sortBooks();
+
+    void sortStudents();
+
+}
