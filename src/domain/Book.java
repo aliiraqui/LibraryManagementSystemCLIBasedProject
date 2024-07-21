401c1087c56fc770b28c16ec1fde0c7060b7eeaf
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-07-21 14:30:02 +0530

    Created Book Class

---
+++ src/domain/Book.java
@@ -0,0 +1,78 @@
+package domain;
+
+public class Book extends AbstractEntity implements Comparable<Book>{
+
+    private final String title;
+    private final String author;
+    private final String publisher;
+    private final String genre;
+    private final String language;
+    private final int numberOfPage;
+    private boolean isaAvailable;
+    private final String borrowerName;
+
+    public Book(String title, String author, String publisher, String genre, String language, int numberOfPage, String borrowerName) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.genre = genre;
+        this.language = language;
+        this.numberOfPage = numberOfPage;
+        this.borrowerName = borrowerName;
+        this.isaAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public boolean isIsaAvailable() {
+        return isaAvailable;
+    }
+
+    public void setIsaAvailable(boolean isaAvailable) {
+        this.isaAvailable = isaAvailable;
+    }
+
+    public String getBorrowerName() {
+        return borrowerName;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("~~~ Book ~~~" +
+                "Title: "+title+
+                "Author: "+author+
+                "Publisher: "+publisher+
+                "Genre: "+genre+
+                "Language: "+language+
+                "Number Of Page: "+numberOfPage+
+                "Is Available? "+isaAvailable+
+                "Borrower Name: "+borrowerName);
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return this.title.compareTo(o.title);
+    }
+}
